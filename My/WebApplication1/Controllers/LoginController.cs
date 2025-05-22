b9d0f6c63e83aab5b1e1a795352d8172569c4f51
tina0928391507 <881104tina@gmail.com>
2025-05-22 11:52:48 +0800

    5/22上午課(後端)

---
+++ My/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(WebApplication1.Models.Longin login)
+        {
+            //1.抓表單送來的帳密
+            //2.檢查帳密是否正確
+
+            //3.如果正確就導入後台頁面
+            //4.如果錯誤就導入登入頁面，並顯示錯誤訊息
+            //假設帳號為admin,密碼為12345678
+
+            if ((login.UserName == "admin" && login.Password == "12345678"))
+            {
+                //導向後台頁面
+                //
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            ViewData["Error"] = "帳密錯誤!!";
+            return View();
+        }
+    }
+}
+
