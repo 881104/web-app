3c68d7eff9a5362c1a416a6ea35eabec53ef7908
tina0928391507 <881104tina@gmail.com>
2025-06-25 15:01:59 +0800

    Teacher重建

--- Teacher/MyController/MyController/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyController.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
