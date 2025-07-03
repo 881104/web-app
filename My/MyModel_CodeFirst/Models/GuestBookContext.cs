e39ca2a8c617a03112b06fde0d36243131f6d7b4
tina0928391507 <881104tina@gmail.com>
2025-07-03 11:39:15 +0800

    7/3待補課

--- My/MyModel_CodeFirst/Models/GuestBookContext.cs
+++ My/MyModel_CodeFirst/Models/GuestBookContext.cs
@@ -17,5 +17,20 @@ namespace MyModel_CodeFirst.Models
         public virtual DbSet<Book> Book { get; set; } //Book資料表
         public virtual DbSet<ReBook> ReBook { get; set; } //ReBook資料表
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //1.2.6
+
+            // 可在此處進行模型配置，例如設定主鍵、索引等
+            modelBuilder.Entity<Book>(E)
+                .HasKey(b => b.BookID);
+            modelBuilder.Entity<ReBook>()
+                .HasKey(r => r.ReBookID);
+            // 設定Book與ReBook之間的一對多關聯
+            modelBuilder.Entity<Book>()
+                .HasMany(b => b.ReBooks)
+                .WithOne(r => r.Book)
+                .HasForeignKey(r => r.BookID);
+        }
     }
 }
